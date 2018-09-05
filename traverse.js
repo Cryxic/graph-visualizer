c1a0e41dc0016fedefbfab099bb643bea999c32e
Alex <36034813+Cryxic@users.noreply.github.com>
2018-09-05 10:05:42 +0300

    Added BFS function

--- traverse.js
+++ traverse.js
@@ -5,5 +5,41 @@ function dfs() {
 }
 
 function bfs() {
-  alert('Breadth-first search not implemented yet');
+  //alert('Breadth-first search not implemented yet');
+  const start = world.board.getCell(world.config.start.row, world.config.start.col);
+  
+  const cellsQueue = [start];
+
+  traverse(cellsQueue);
+}
+
+function traverse(cellsToVisit) {
+  if (cellsToVisit.length === 0 || world.board.endReached) {
+    return;
+  }
+
+  const cell = cellsToVisit.shift();
+
+  console.log('Visiting (%d, %d)', cell.row, cell.col);
+  cell.status = CellStatus.CLOSED;
+
+  if (cell.row === world.config.row && cell.col === world.config.end) {
+    console.log('The end was reached');
+    world.board.endReached = true;
+    colorFoundWay();
+  }
+
+  const neighbors = getNeighbors(cell);
+
+  for (var i = 0; i < neighbors.length; i++) {
+    const neighbor = neighbors[i];
+    if (neighbor.status == CellStatus.UNREACHED) {
+      cellsToVisit.push(neighbor);
+      neighbor.status = CellStatus.OPEN;
+      neighbor.parent = cell;
+    }
+  }
+  setTimeout(() => {
+    traverse(cellsToVisit);
+  }, world.config.stepWait);
 }
